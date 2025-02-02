617e6c53953bd64ff190e8e5b1e6c84476ab8cd9
vrtxp3 <K.S.Andropov@gmail.com>
2025-02-02 11:24:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-
 function convertDate(dateString) {
-  
   const formatRegex = /^(?:\d{4}-\d{1,2}-\d{1,2})|(?:\d{2}\/\d{2}\/\d{4})$/;
   if (!formatRegex.test(dateString)) {
-    return "Формат даты должен быть YYYY-MM-DD или DD/MM/YYYY/";
+    return "Формат даты должен быть YYYY-MM-DD или DD/MM/YYYY";
   }
 
-  
+  // Удаление лишних символов, оставляя только цифры и разделители
+  dateString = dateString.replace(/[^\d\/\-]/g, '');
+
   let day, month, year;
   if (dateString.includes("-")) {
     [year, month, day] = dateString.split("-");
@@ -14,10 +14,8 @@ function convertDate(dateString) {
     [day, month, year] = dateString.split("/");
   }
 
-  
   month = parseInt(month) - 1;
 
-  
   const months = [
     "января",
     "февраля",
@@ -34,31 +32,23 @@ function convertDate(dateString) {
   ];
   const monthName = months[month];
 
-  
-  day = day.replace(/^0+/, ""); 
+  day = day.replace(/^0+/, "");
   const formattedDate = `${day} ${monthName} ${year}`;
 
   return formattedDate;
 }
 
-
 const dateInput1 = document.getElementById("dateInput1");
 const convertedDate1Element = document.getElementById("convertedDate1");
 
-
 dateInput1.addEventListener("input", () => {
- 
   const dateString = dateInput1.value;
- 
+
   const convertedDate = convertDate(dateString);
 
-  
   convertedDate1Element.textContent = convertedDate;
- 
 
-  
-  if (convertedDate !== "Формат даты должен быть YYYY-MM-DD или DD/MM/YYYY/") {
+  if (convertedDate !== "Формат даты должен быть YYYY-MM-DD или DD/MM/YYYY") {
     navigator.clipboard.writeText(convertedDate1Element.textContent);
   }
- 
 });
